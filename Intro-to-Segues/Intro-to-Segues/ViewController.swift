dd20cf562d56e2a8dd69d1c9dee5703617ed3243
Simone Good <110115924+simonegood7@users.noreply.github.com>
2022-07-28 10:20:01 -0400

    IntroToSegues

---
+++ Intro-to-Segues/Intro-to-Segues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Intro-to-Segues
+//
+//  Created by scholar on 7/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
